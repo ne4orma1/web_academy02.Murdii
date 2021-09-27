32468d984282c7ff1b7cd1f974526f0881766be2
ne4orma1 <maksrabota12@gmail.com>
2021-09-27 21:18:25 +0300

    Create minio bucket

---
+++ minio/variables.tf
@@ -0,0 +1,19 @@
+variable "minio_region" {
+  description = "Default minio region"
+  default     = "us-east-1"
+}
+
+variable "minio_server" {
+  description = "Default minio host and port"
+  default     = "192.168.0.106:9000"
+}
+
+variable "minio_access_key" {
+  description = "minio user"
+  default     = "root"
+}
+
+variable "minio_secret_key" {
+  description = "minio_secret_key"
+  default     = "maks150750"
+}
